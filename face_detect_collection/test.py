ea478ce6dee7a1db5e74e8c5324025c9f538f27a
GrishkaZ <grigoriozaly@yandex.ru>
2023-10-28 21:11:04 +0300

    init5

--- face_detect_collection/test.py
+++ face_detect_collection/test.py
@@ -1,7 +1,8 @@
 def hello_world():
+    import cv2
+    print(cv2.__version__)
     print('I\'s works!')
 
 if __name__ == '__main__':
-    import cv2
-    print(cv2.__version__)
+
     hello_world()
