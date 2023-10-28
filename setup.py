92f741d80e8ec80fb30f9ad417a01b4bd6846144
GrishkaZ <grigoriozaly@yandex.ru>
2023-10-28 22:17:10 +0300

    resources

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     packages=find_packages(),
     install_requires=requirements,
     package_data={
-        "face_detect_collection": ['models/*'],
+        "face_detect_collection": ['models/face_detection_models/*'],
     },
     # include_package_data=True,
 )
