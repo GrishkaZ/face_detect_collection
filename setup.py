31e3af5120d4ce61dc4e8944830a9c7b9fd7c6df
GrishkaZ <grigoriozaly@yandex.ru>
2023-10-28 20:40:34 +0300

    init

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+from face_detect_collection import __version__
+
+setup(
+    name='face_detect_collection',
+    version=__version__,
+
+    url='https://github.com/MichaelKim0407/tutorial-pip-package',
+    author='GZ',
+    author_email='grigoriozaly@yandex.ru',
+
+    py_modules=['face_detect_collection'],
+)
