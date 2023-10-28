9e59b2894851c3f1eff0d9401f875b248c8a2b14
GrishkaZ <grigoriozaly@yandex.ru>
2023-10-29 01:53:46 +0300

    fix __init__

--- face_detect_collection/__init__.py
+++ face_detect_collection/__init__.py
@@ -1,6 +1,7 @@
 from .face_detection import CaffeFaceDetector, HaarCascadeFaceDetector
 from  .face_embeddings import IResnet
+from  .face_lanmarks import MediapipeFaceMeshDetector
 
-__all__ = ["face_detection", 'face_embeddings']
+__all__ = ['face_lanmarks', 'face_detection', 'face_embeddings']
 
 __version__ = '0.0.2'
